1b5944722244d08f050b9c4e45cb384509a50d5d
Leander <leander@dev.leander.net>
2023-11-27 15:18:30 +0100

    fifth commit

--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,11 @@
 #include "include/parser.h"
+#include "include/lexer.h"
+
+parser_T *init_parser(lexer_T *lexer) {}
+
+void parser_eat(parser_T *parser, int token_type) {}
+
+AST_T parser_parse(parser_T *parser) {}
+
+AST_T *parser_parse_statement(parser_T *parser) {}
+
